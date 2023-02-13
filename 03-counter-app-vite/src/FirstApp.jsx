60c084a4675b952de1707ec9526a21faddbbca24
AlexDevv <104278094+AlexProcess@users.noreply.github.com>
2023-02-13 16:21:37 +0100

    create a component

---
+++ 03-counter-app-vite/src/FirstApp.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const AlexFirstComponent = () => {
+  return (
+    <h1>Alex First Component</h1>
+  )
+}
--- 03-counter-app-vite/src/main.jsx
+++ 03-counter-app-vite/src/main.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import reactDOM from 'react-dom/client';
+import { FirstApp } from './FirstApp';
 
 import { HelloWorldApp } from './HelloWorldApp'
 
@@ -7,5 +8,6 @@ import { HelloWorldApp } from './HelloWorldApp'
 reactDOM.createRoot( document.getElementById('root') ).render(
     <React.StrictMode>
         <HelloWorldApp/>
+        <FirstApp/>
     </React.StrictMode>
 );

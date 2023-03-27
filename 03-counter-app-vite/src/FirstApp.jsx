7002423fc1706420ace65eefa5395c943aeaaaf7
AlexDevv <104278094+AlexProcess@users.noreply.github.com>
2023-03-27 11:37:20 +0200

    Props

--- 03-counter-app-vite/src/FirstApp.jsx
+++ 03-counter-app-vite/src/FirstApp.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
+import React from "react";
 
-export const FirstApp = () => {
-    return(
-    <>
-      <h1> Alex </h1>
-      <p>Soy un subtitulo</p>
-    </>)
-}
-  
+export const FirstApp = (props) => {
+    const { title = "Hola, soy Alex =)" } = props;
+    const { subTitle = "11111" } = props;
+
+    return (
+        <>
+            <h1>{title}</h1>
+            <p>{subTitle + 1}</p>
+        </>
+    );
+};
--- 03-counter-app-vite/src/main.jsx
+++ 03-counter-app-vite/src/main.jsx
@@ -1,12 +1,11 @@
-import React from 'react';
-import reactDOM from 'react-dom/client';
-import './styles.css';
-import { FirstApp } from './FirstApp';
-import { HelloWorldApp } from './HelloWorldApp'
+import React from "react";
+import reactDOM from "react-dom/client";
+import "./styles.css";
+import { FirstApp } from "./FirstApp";
+import { HelloWorldApp } from "./HelloWorldApp";
 
-
-reactDOM.createRoot( document.getElementById('root') ).render(
+reactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
-        <FirstApp/>
+        <FirstApp title="Hola, soy AlexDev =)" />
     </React.StrictMode>
 );

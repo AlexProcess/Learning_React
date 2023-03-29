189dc1f2e68939b29a76c4cb75c0a746b63f2dd6
AlexDevv <104278094+AlexProcess@users.noreply.github.com>
2023-03-29 21:01:15 +0200

    pruebas en 02-template string

---
+++ 03-counter-app-vite/src/tests/BasePruebas/02-template-string.test.js
@@ -0,0 +1,8 @@
+describe("Pruebas en 02-template-string", () => {
+    test("getSaludo debe retornar 'Hola Alex'", () => {
+        const name = "Alex";
+        const message = getSaludo(name);
+
+        expect(message).toBe("Hola ${name}!!!");
+    });
+});

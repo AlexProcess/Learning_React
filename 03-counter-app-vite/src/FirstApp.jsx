16b20f9223f1c33064d4f2391aa80b12a5d78108
AlexDevv <104278094+AlexProcess@users.noreply.github.com>
2023-04-16 12:49:16 +0200

    fix and finish screen testing library

--- 03-counter-app-vite/src/FirstApp.jsx
+++ 03-counter-app-vite/src/FirstApp.jsx
@@ -3,7 +3,7 @@ import { PropTypes } from "prop-types";
 
 export const FirstApp = (props) => {
     const { title = "Hola soy Alex" } = props;
-    const { subTitle = "El diablo puñeeeta" } = props;
+    const { subTitle = "Soy un subtitulos" } = props;
     const { nombre = "" } = props;
 
     // if (!title) {
@@ -13,6 +13,7 @@ export const FirstApp = (props) => {
     return (
         <>
             <h1 data-testid="test-title">{title}</h1>
+            <p>{subTitle}</p>
             <p>{subTitle}</p>
             <p>{nombre}</p>
         </>
--- 03-counter-app-vite/src/tests/FirstApp2.test.jsx
+++ 03-counter-app-vite/src/tests/FirstApp2.test.jsx
@@ -1,21 +1,29 @@
-import { render, screen } from "@testing-library/react";
+import { getAllByText, render, screen } from "@testing-library/react";
 import { FirstApp } from "../FirstApp";
 
 describe("Pruebas en FirstApp.jsx", () => {
+    const title = "Hola, soy goku";
+    const subTitle = "Soy un subtitulo";
+
     test("La PRIMERA PRUEBA DEBE DE HACER MATCH CON SNAPSHOT", () => {
-        const title = "Hola, soy goku";
         const { container } = render(<FirstApp title={title} />);
         expect(container).toMatchSnapshot();
     });
     test("debe de mostrar el mensaje'Hola, soy Gokuy'", () => {
-        const title = "Hola, soy goku";
         render(<FirstApp title={title} />);
-        expect(screen.getByText(title).toBeTruthy());
+        expect(screen.getByText(title)).toBeTruthy();
     });
 
     test("debe de mostrar un titulo que este en un h1", () => {
-        const title = "Hola, soy goku";
         render(<FirstApp title={title} />);
-        expect(screen.getByRole("heading", { level: 1 }).innerHTML).toContain();
+        expect(screen.getByRole("heading", { level: 1 }).innerHTML).toContain(
+            title
+        );
+    });
+
+    test("debe de mostrar el subtitulo enviado por props", () => {
+        render(<FirstApp title={title} subTitle={subTitle} />);
+
+        expect(screen.getAllByText(subTitle).length).toBe(2);
     });
 });

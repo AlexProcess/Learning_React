b5086e164c87327558a3e6e1b79837ce7f8973f6
AlexDevv <104278094+AlexProcess@users.noreply.github.com>
2023-04-19 07:35:02 +0200

    category list

--- 04-gif-expert-app/src/GifExpertApp.jsx
+++ 04-gif-expert-app/src/GifExpertApp.jsx
@@ -1,7 +1,24 @@
+import { useState } from "react";
+
 export const GifExpertApp = () => {
+    const [categories, setCategories] = useState(["One Punch"]);
+
+    console.log();
+
     return (
         <>
+            {/* { Titutlo } */}
             <h1>GifExpertApp</h1>
+
+            {/* {Input} */}
+            <ol>
+                {categories.map((category) => {
+                    return <li key={category}>Hola</li>;
+                })}
+                {/* <li>Abc</li> */}
+            </ol>
+
+            {/* {Gif item} */}
         </>
     );
 };

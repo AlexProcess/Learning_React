0cab073cd3dda33f4812cac5ca9a1002eeccc5c9
AlexDevv <104278094+AlexProcess@users.noreply.github.com>
2023-04-19 19:46:42 +0200

    Emitir un evento padre

--- 04-gif-expert-app/src/GifExpertApp.jsx
+++ 04-gif-expert-app/src/GifExpertApp.jsx
@@ -4,8 +4,9 @@ import { AddCategory } from "./Components/AddCategory";
 export const GifExpertApp = () => {
     const [categories, setCategories] = useState(["One Punch", "Dragon Ball"]);
 
-    const onAddCategory = () => {
-        setCategories([...categories, "Valorant"]);
+    const onAddCategory = (newCategory) => {
+        console.log(newCategory);
+        setCategories([newCategory, ...categories]);
         // setCategories((cat) => [...cat, "Valorant"]);
     };
 
@@ -15,7 +16,11 @@ export const GifExpertApp = () => {
             <h1>GifExpertApp</h1>
 
             {/* {Input} */}
-            <AddCategory setCategories={setCategories} />
+            <AddCategory
+                /*setCategories={setCategories}*/ onNewCategory={(value) =>
+                    onAddCategory(value)
+                }
+            />
             {/* {Listado del gif} */}
             <ol>
                 {categories.map((category) => {

0fc58f4ba3bb08bf4c33cbdd2085992e95354543
AlexDevv <104278094+AlexProcess@users.noreply.github.com>
2023-05-28 20:40:13 +0200

    Test en getgifs

--- 04-gif-expert-app/test/components/GifItem.test.jsx
+++ 04-gif-expert-app/test/components/GifItem.test.jsx
@@ -1,12 +1,26 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { GifItem } from "../../src/Components/GifItem";
 
 describe("Pruebas en <GifItem/>", () => {
-    const title = "Anuel";
-    const url = "https://anuelAA/anuel1millon.jpg.com";
+    const title = "Saitama";
+    const url = "https://anuelaa/saitama.jpg.com";
 
     test("debe de hacer match con el snapshot", () => {
-        render(<GifItem />);
-        expect(container).toMatchSnapshot(url);
+        const { container } = render(<GifItem title="" url="" />);
+        expect(container).toMatchSnapshot();
+    });
+
+    test("debe de mostrar la imgaen indicada con la url indicada", () => {
+        render(<GifItem title={title} url={url} />);
+        // screen.debug();
+        // expect(screen.getByRole("img").src);
+        const { src, alt } = screen.getByRole("img");
+        expect(src).toBe(url);
+        expect(src).toBe(alt);
+    });
+    test("debe de mostrar el titulo del componente", () => {
+        render(<GifItem title={title} url={url} />);
+
+        expect(screen.getByText(title)).toBeTruthy();
     });
 });

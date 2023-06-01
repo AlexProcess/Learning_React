9158805446845eb5c99bcf4ede89e1c1c35a8b1f
AlexDevv <104278094+AlexProcess@users.noreply.github.com>
2023-06-01 09:00:35 +0200

    finish testing section

--- 04-gif-expert-app/test/components/GifExpertApp.test.jsx
+++ 04-gif-expert-app/test/components/GifExpertApp.test.jsx
@@ -2,8 +2,12 @@ import { render } from "@testing-library/react";
 import { GifExpertApp } from "../../src/GifExpertApp";
 
 describe("Pruebas en <GifExpertApp/>", () => {
+    const category = "AnuelAA";
+
     test("", () => {
         render(<GifExpertApp />);
         screen.debug(undefined, Infinity);
+
+        newCategory = category;
     });
 });

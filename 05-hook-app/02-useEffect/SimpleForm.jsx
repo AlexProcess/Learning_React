7287d806ae7a9880ed083901692ed044e5314ed2
AlexDevv <104278094+AlexProcess@users.noreply.github.com>
2023-06-06 11:22:07 +0200

    dependencias del useEffect

---
+++ 05-hook-app/02-useEffect/SimpleForm.jsx
@@ -0,0 +1,47 @@
+import { useEffect, useState } from "react";
+
+export const SimpleForm = () => {
+    const [formState, setFormState] = useState({
+        username: "",
+        email: "",
+    });
+
+    const { username, email } = formState;
+
+    const onInputChange = ({ target }) => {
+        const { name, value } = target;
+        setFormState({
+            ...formState,
+            [name]: value,
+        });
+    };
+
+    useEffect(() => {
+        console.log("useEffect called!");
+    });
+
+    return (
+        <>
+            <h1>Simple Form</h1>
+
+            <hr />
+
+            <input
+                type="text"
+                className="form-control"
+                placeholder="Username"
+                name="username"
+                value={username}
+                onChange={onInputChange}
+            />
+            <input
+                type="email"
+                className="form-control mt-3"
+                placeholder="alex@google.com"
+                name="email"
+                value={email}
+                onChange={onInputChange}
+            />
+        </>
+    );
+};

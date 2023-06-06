3e7cd833c7e246fc0b59e109bacc93ec163ee411
AlexDevv <104278094+AlexProcess@users.noreply.github.com>
2023-06-06 10:13:43 +0200

    exponer metodos del hook

--- 05-hook-app/01-useState/CounterWithCustomHook.jsx
+++ 05-hook-app/01-useState/CounterWithCustomHook.jsx
@@ -1,7 +1,7 @@
 import { useCounter } from "../hooks/useCounter";
 
 export const CounterWithCustomHook = () => {
-    const { counter } = useCounter();
+    const { counter, increment, decrement, reset } = useCounter();
 
     return (
         <>
@@ -9,9 +9,15 @@ export const CounterWithCustomHook = () => {
 
             <hr />
 
-            <button className="btn btn-primary">+1</button>
-            <button className="btn btn-primary">reset</button>
-            <button className="btn btn-primary">-1</button>
+            <button className="btn btn-primary" onClick={increment}>
+                +1
+            </button>
+            <button className="btn btn-primary" onClick={reset}>
+                reset
+            </button>
+            <button className="btn btn-primary" onClick={decrement}>
+                -1
+            </button>
         </>
     );
 };

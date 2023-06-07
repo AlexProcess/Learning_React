429d591d9916026a89a9852c09ded082ac798355
AlexDevv <104278094+AlexProcess@users.noreply.github.com>
2023-06-07 15:18:51 +0200

    Conectando ambos hooks

--- 05-hook-app/03-examples/MultipleCustomHooks.jsx
+++ 05-hook-app/03-examples/MultipleCustomHooks.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { useFetch } from "../hooks/useFetch";
+import { useCounter } from "../hooks/useCounter";
 
 export const MultipleCustomHooks = () => {
+    const { counter, increment } = useCounter(1);
     const { data, isLoading, hasError } = useFetch(
-        `https://api.breakingbadquotes.xyz/v1/quotes/5`
+        `https://api.breakingbadquotes.xyz/v1/quotes/${counter}`
     );
 
     const { author, quote } = !!data && data[0];
@@ -28,7 +30,13 @@ export const MultipleCustomHooks = () => {
                     <footer className="blockquote-footer mt-1">{author}</footer>
                 </blockquote>
             )}
-            <button className="btn btn-primary">Next quote</button>
+            <button
+                disabled={isLoading}
+                onClick={() => increment(1)}
+                className="btn btn-primary"
+            >
+                Next quote
+            </button>
         </>
     );
 };

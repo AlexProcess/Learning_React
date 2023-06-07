bbd8e6e12c95db738cc3a6dc137f2372cdda7c5b
AlexDevv <104278094+AlexProcess@users.noreply.github.com>
2023-06-07 15:12:13 +0200

    useFetch + useCounter

--- 05-hook-app/03-examples/MultipleCustomHooks.jsx
+++ 05-hook-app/03-examples/MultipleCustomHooks.jsx
@@ -6,11 +6,29 @@ export const MultipleCustomHooks = () => {
         `https://api.breakingbadquotes.xyz/v1/quotes/5`
     );
 
-    console.log(data, isLoading, hasError);
+    const { author, quote } = !!data && data[0];
+    // si la data !!data y && la primera de posicion de data es decir data[0]
+
+    console.log(data);
+
+    // if (isLoading === true) {
+    //     return <h1>Cargando...</h1>;
+    // }
 
     return (
         <>
             <h1>BreakingBad Quotes</h1>
+            <hr />
+
+            {isLoading ? (
+                <div className="alert alert-info text-center">Loading</div>
+            ) : (
+                <blockquote className="blockquote text-end">
+                    <p className="mb-1">{quote}</p>
+                    <footer className="blockquote-footer mt-1">{author}</footer>
+                </blockquote>
+            )}
+            <button className="btn btn-primary">Next quote</button>
         </>
     );
 };

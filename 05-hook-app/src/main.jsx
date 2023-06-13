b6aca3f956885892effff9a56800fc4861f48ccb
AlexDevv <104278094+AlexProcess@users.noreply.github.com>
2023-06-13 21:03:27 +0200

    Memo

--- 05-hook-app/src/main.jsx
+++ 05-hook-app/src/main.jsx
@@ -3,10 +3,11 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 // import { MultipleCustomHooks } from "../03-examples/MultipleCustomHooks";
 // import { FocusScreen } from "../04-useRef/FocusScreen";
-import { Layout } from "../05-useLayoutEffect/Layout";
+// import { Layout } from "../05-useLayoutEffect/Layout";
+import { Memorize } from "../06-memos/Memorize";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <>
-        <Layout />
+        <Memorize />
     </>
 );

f79a1ccb08b413280e68105bbd5f52653b44bad8
AlexDevv <104278094+AlexProcess@users.noreply.github.com>
2023-06-18 00:56:40 +0200

    Toggle ToDo marcar como completa

--- 05-hook-app/08-useReducer/TodoApp.jsx
+++ 05-hook-app/08-useReducer/TodoApp.jsx
@@ -37,6 +37,13 @@ export const TodoApp = () => {
         });
     };
 
+    const handleToggleTodo = (id) => {
+        dispatch({
+            type: "[TODO] Toggle todo",
+            payload: id,
+        });
+    };
+
     return (
         <>
             <h1>
@@ -46,7 +53,11 @@ export const TodoApp = () => {
             <div className="row">
                 <div className="col-7">
                     {/* {TodoList} */}
-                    <TodoList todos={todos} onDeleteTodo={handleDeleteTodo} />
+                    <TodoList
+                        todos={todos}
+                        onDeleteTodo={handleDeleteTodo}
+                        onToggleTodo={handleToggleTodo}
+                    />
                     {/* {TodoList} */}
                 </div>
                 <div className="col-5">
--- 05-hook-app/08-useReducer/todoReducer.js
+++ 05-hook-app/08-useReducer/todoReducer.js
@@ -6,6 +6,18 @@ export const todoReducer = (initialState, action) => {
         case "[TODO] Delete todo":
             return initialState.filter((todo) => todo.id !== action.payload);
 
+        case "[TODO] Toggle todo":
+            return initialState.map((todo) => {
+                if (todo.id === action.payload) {
+                    return {
+                        ...todo,
+                        done: !todo.done,
+                    };
+                }
+
+                return todo;
+            });
+
         default:
             return initialState;
     }

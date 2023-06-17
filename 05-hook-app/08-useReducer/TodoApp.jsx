3f8eed89642743bef0b2ca40e5029bf8c3f9c41a
AlexDevv <104278094+AlexProcess@users.noreply.github.com>
2023-06-18 01:18:12 +0200

    useToDo --> UseTodo {isDone: True =)}

--- 05-hook-app/08-useReducer/TodoApp.jsx
+++ 05-hook-app/08-useReducer/TodoApp.jsx
@@ -2,46 +2,15 @@ import { useEffect, useReducer } from "react";
 import { todoReducer } from "./todoReducer";
 import { TodoAdd } from "./TodoAdd";
 import { TodoList } from "./TodoList";
+import { useTodo } from "../hooks";
 
-const initialState = [];
-
-const init = () => {
-    let dataInLocal = localStorage.getItem("task");
-    return dataInLocal ? JSON.parse(dataInLocal) : [];
-};
 export const TodoApp = () => {
-    const { todos, handleDeleteTodo, handleToggleTodo, HandleNewTodo } =
+    const { todos, handleDeleteTodo, handleToggleTodo, handleNewTodo } =
         useTodo();
-    const [todos, dispatch] = useReducer(todoReducer, initialState, init);
-
-    //useTodo
-    //todos, handleDeleteTodo, handleToggleTodo, HandleNewTodo
 
     useEffect(() => {
         localStorage.setItem("todos", JSON.stringify(todos));
     }, [todos]);
-
-    const handleNewTodo = (todo) => {
-        const action = {
-            type: "[TODO] Add todo",
-            payload: todo,
-        };
-        dispatch(action);
-    };
-
-    const handleDeleteTodo = (id) => {
-        dispatch({
-            type: "[TODO] Delete todo",
-            payload: id,
-        });
-    };
-
-    const handleToggleTodo = (id) => {
-        dispatch({
-            type: "[TODO] Toggle todo",
-            payload: id,
-        });
-    };
 
     return (
         <>
---
+++ 05-hook-app/hooks/useTodo.js
@@ -0,0 +1,44 @@
+import { useEffect, useReducer } from "react";
+import { todoReducer } from "../08-useReducer/todoReducer";
+
+const init = () => {
+    let dataInLocal = localStorage.getItem("task");
+    return dataInLocal ? JSON.parse(dataInLocal) : [];
+};
+
+export const useTodo = () => {
+    const initialState = [];
+    const [todos, dispatch] = useReducer(todoReducer, initialState, init);
+
+    useEffect(() => {
+        localStorage.setItem("todos", JSON.stringify(todos));
+    }, [todos]);
+
+    const handleNewTodo = (todo) => {
+        const action = {
+            type: "[TODO] Add todo",
+            payload: todo,
+        };
+        dispatch(action);
+    };
+
+    const handleDeleteTodo = (id) => {
+        dispatch({
+            type: "[TODO] Delete todo",
+            payload: id,
+        });
+    };
+
+    const handleToggleTodo = (id) => {
+        dispatch({
+            type: "[TODO] Toggle todo",
+            payload: id,
+        });
+    };
+    return {
+        handleNewTodo,
+        handleToggleTodo,
+        handleDeleteTodo,
+        todos,
+    };
+};

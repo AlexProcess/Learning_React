0c23a5147670259b9531ff09184e797084e44f82
AlexDevv <104278094+AlexProcess@users.noreply.github.com>
2023-06-18 20:37:43 +0200

    preparacion de nuestra aplicación con rutas

--- 05-hook-app/src/main.jsx
+++ 05-hook-app/src/main.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { TodoApp } from "../08-useReducer/TodoApp";
+import { MainApp } from "../09-UseContext/MainApp";
+import {
+    BrowserRouter,
+    createBrowserRouter,
+    RouterProvider,
+} from "react-router-dom";
 // import { MultipleCustomHooks } from "../03-examples/MultipleCustomHooks";
 // import { FocusScreen } from "../04-useRef/FocusScreen";
 // import { Layout } from "../05-useLayoutEffect/Layout";
@@ -12,6 +17,8 @@ import { TodoApp } from "../08-useReducer/TodoApp";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <>
-        <TodoApp />
+        <BrowserRouter>
+            <MainApp />
+        </BrowserRouter>
     </>
 );

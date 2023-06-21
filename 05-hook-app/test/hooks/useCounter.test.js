7370add6fbc2738b7c7703959dab56cab5bfafa5
AlexDevv <104278094+AlexProcess@users.noreply.github.com>
2023-06-21 11:01:44 +0200

    ejecutar funciones del custom hook dentro de los test

--- 05-hook-app/test/hooks/useCounter.test.js
+++ 05-hook-app/test/hooks/useCounter.test.js
@@ -1,5 +1,6 @@
 import { renderHook } from "@testing-library/react";
 import { useCounter } from "../../hooks";
+import { act } from "react-dom/test-utils";
 
 describe("Pruebas en el UseCounter", () => {
     test("Debe de retornar los valores por defecto", () => {
@@ -16,5 +17,38 @@ describe("Pruebas en el UseCounter", () => {
         const { counter } = result.current;
 
         expect(counter).toBe(100);
+    });
+
+    test("debe de incrementar el contador", () => {
+        const { result } = renderHook(() => useCounter(100));
+        const { increment } = result.current;
+
+        act(() => {
+            increment(3);
+        });
+
+        expect(result.current.counter).toBe(103);
+    });
+
+    test("debe de decrementar el contador", () => {
+        const { result } = renderHook(() => useCounter(100));
+        const { decrement } = result.current;
+
+        act(() => {
+            decrement(3);
+        });
+
+        expect(result.current.counter).toBe(97);
+    });
+
+    test("debe de resetear el contador", () => {
+        const { result, initialValue } = renderHook(() => useCounter());
+        const { reset } = result.current;
+
+        act(() => {
+            reset(initialValue);
+        });
+
+        expect(result.current.counter).toBe(10);
     });
 });

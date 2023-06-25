fcdf1694c822823629a27ccc06d2e9a006722ab0
AlexDevv <104278094+AlexProcess@users.noreply.github.com>
2023-06-25 22:51:05 +0200

    Pruebas generales en nuestro App Router

--- 05-hook-app/09-UseContext/MainApp.jsx
+++ 05-hook-app/09-UseContext/MainApp.jsx
@@ -8,7 +8,7 @@ import { UserProvider } from "./context/UserProvider";
 export const MainApp = () => {
     return (
         <UserProvider>
-            <h1>MainApp</h1>
+            <h1>HomePage</h1>
             <hr />
 
             <Navbar />

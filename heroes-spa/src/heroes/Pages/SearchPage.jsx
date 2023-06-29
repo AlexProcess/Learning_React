19ccea4d78e1adb5069d60a7baf520c8123ba463
AlexDevv <104278094+AlexProcess@users.noreply.github.com>
2023-06-29 21:13:11 +0200

    Mostrar mensajes condicionales

--- heroes-spa/src/heroes/Pages/SearchPage.jsx
+++ heroes-spa/src/heroes/Pages/SearchPage.jsx
@@ -15,12 +15,15 @@ export const SearchPage = () => {
         searchText: '',
     })
 
+    
     const onSearchSubmmit = (event) => {
         event.preventDefault();
         if (searchText.trim().length <= 1) return;
         
         navigate(`/search/?q=${searchText.toLowerCase().trim()}`);
     }
+    const showSearch = (q.length === 0);
+    const errorSearch = (q.length > 0 && heroes.length === 0);
     
     return (
         <>
@@ -47,12 +50,25 @@ export const SearchPage = () => {
             <div className="col-7">
                 <h4>Results</h4>
                 <hr/>
-                <div className="alert alert-primary">
-                    Search a hero
-                </div>
-                <div className="alert alert-danger">
-                    No hero with <b>{q}</b>
-                </div>
+
+                {/* {
+                    (q === "")
+                    ? <div className="alert alert-primary">
+                    Search a hero 
+                    </div> 
+                    : (heroes.length === 0 ) 
+                    &&  <div className="alert alert-danger">No hero with <b>{q}</b></div>
+                } */}
+
+                <div className="alert alert-primary animate__animated animate__fadeIn" style={{display: showSearch ? "" :  "none" }}>
+                    Search a hero 
+                </div> 
+
+                <div className="alert alert-danger animate__animated animate__fadeIn" style={{display: errorSearch ?  "" : "none"   }}>
+                No hero with <b>{q}</b>
+                </div> 
+                
+                
                 {
                     heroes.map(hero => (
                         <HeroCard 

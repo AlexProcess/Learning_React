eca7795d6fbf7fd7faa841a7577e38e495d81662
AlexDevv <104278094+AlexProcess@users.noreply.github.com>
2023-07-05 11:24:16 +0200

    Rutas privadas

--- heroes-spa/src/Router/AppRouter.jsx
+++ heroes-spa/src/Router/AppRouter.jsx
@@ -2,13 +2,19 @@ import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import { LoginPage } from "../auth/Pages/LoginPage";
 import {} from "module";
 import { HeroesRoutes } from "../heroes/routes/HeroesRoutes";
+import { PrivateRoute } from "./PrivateRoute";
 export const AppRouter = () => {
     return (
         <>
             <BrowserRouter>
                 <Routes>
                     <Route path="login" element={<LoginPage />} />
-                    <Route path="/*" element={<HeroesRoutes />} />
+                    <Route path="/*" element={
+                        <PrivateRoute>
+                            <HeroesRoutes />
+                        </PrivateRoute>
+                    }/>
+
                 </Routes>
             </BrowserRouter>
         </>

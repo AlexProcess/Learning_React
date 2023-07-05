fd063ac5753dc8c440f1f690d5b844e683c7ac2e
AlexDevv <104278094+AlexProcess@users.noreply.github.com>
2023-07-05 11:38:02 +0200

    rutas publicas

--- heroes-spa/src/Router/AppRouter.jsx
+++ heroes-spa/src/Router/AppRouter.jsx
@@ -3,17 +3,23 @@ import { LoginPage } from "../auth/Pages/LoginPage";
 import {} from "module";
 import { HeroesRoutes } from "../heroes/routes/HeroesRoutes";
 import { PrivateRoute } from "./PrivateRoute";
+import { PublicRoute } from "./PublicRoute";
 export const AppRouter = () => {
     return (
         <>
             <BrowserRouter>
                 <Routes>
-                    <Route path="login" element={<LoginPage />} />
-                    <Route path="/*" element={
-                        <PrivateRoute>
-                            <HeroesRoutes />
-                        </PrivateRoute>
-                    }/>
+                <Route path="/login" element={
+                    <PublicRoute>
+                        <LoginPage/>
+                    </PublicRoute>
+                }/>
+                
+                <Route path="/*" element={
+                    <PrivateRoute>
+                        <HeroesRoutes />
+                    </PrivateRoute>
+                }/>
 
                 </Routes>
             </BrowserRouter>

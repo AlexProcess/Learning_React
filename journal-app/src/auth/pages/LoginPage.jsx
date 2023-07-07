11eb6e8cb9f778dcf5f99cee47ea995a1726b2b9
AlexDevv <104278094+AlexProcess@users.noreply.github.com>
2023-07-07 13:45:44 +0200

    login page

--- journal-app/src/auth/pages/LoginPage.jsx
+++ journal-app/src/auth/pages/LoginPage.jsx
@@ -1,7 +1,45 @@
-import React from 'react'
+import { Grid, TextField, Typography } from "@mui/material"
+
 
 export const LoginPage = () => {
   return (
-    <div>LoginPage</div>
+    <Grid 
+    container
+    spacing={ 0 }
+    direction="column"
+    alignItems="center"
+    justifyContent="center"
+    sx={{minHeight: "100vh", backgroundColor: "primary.main", padding: 4}}
+    >
+      <Grid 
+      item 
+      className="box-shadow"
+      xs={ 3 }
+      sx={{backgroundColor: "white", padding: 3, borderRadius: 2}}>
+        <Typography variant="h5" sx={{mb: 1}}>Login</Typography>
+        <form>
+          <Grid container >
+            <Grid item xs={12} sx={{mt:2}}>
+              <TextField 
+              label="Google@.com"
+              type="email"
+              placeholder="CorreoGoogle@gmail.com"
+              fullWidth>
+            </TextField>
+            <Grid item xs={12} sx={{mt:2}}>
+              <TextField 
+              label="Contraseña"
+              type="email"
+              placeholder="Contraseña"
+              fullWidth>
+            </TextField>
+            </Grid>
+            </Grid>
+          </Grid>
+        </form>
+
+      </Grid>
+
+    </Grid>
   )
 }

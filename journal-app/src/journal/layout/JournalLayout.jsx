567a13dd4dac3480093e31d4fbf87da72c8918c8
AlexDevv <104278094+AlexProcess@users.noreply.github.com>
2023-07-08 21:33:34 +0200

    SideBar

--- journal-app/src/journal/layout/JournalLayout.jsx
+++ journal-app/src/journal/layout/JournalLayout.jsx
@@ -1,20 +1,18 @@
-import { Box } from '@mui/material'
-import { NavBar } from '../components'
+import { Box } from "@mui/material";
+import { NavBar, SideBar } from "../components";
 
-export const JournalLayout = ({children}) => {
-  const drawerWith = 240
-  
-  return (
-    <Box sx={{display: 'flex'}}>
-        <NavBar drawerWith={drawerWith}/>
+export const JournalLayout = ({ children }) => {
+    const drawerWith = 240;
 
-        {/* SIDEBAR */}
-        <Box 
-        component="main"
-        sx={{flexGrow: 1, p: 3}}>
-            {/* Toolbar */}
-            {children}
+    return (
+        <Box sx={{ display: "flex" }}>
+            <NavBar drawerWith={drawerWith} />
+
+            <SideBar drawerWidth={drawerWith} />
+            <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+                {/* Toolbar */}
+                {children}
+            </Box>
         </Box>
-    </Box>
-  )
-}
+    );
+};

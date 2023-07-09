82e8c3ff5c8fe193baf227d32c6f41f8d5ce4098
AlexDevv <104278094+AlexProcess@users.noreply.github.com>
2023-07-09 21:05:37 +0200

    Note view finished and fixed

--- journal-app/src/journal/pages/JournalPage.jsx
+++ journal-app/src/journal/pages/JournalPage.jsx
@@ -1,18 +1,11 @@
-import { Typography } from "@mui/material";
-import { MailOutline } from "@mui/icons-material";
 import { JournalLayout } from "../layout/JournalLayout";
+import { NoteView, NothingSelectedView } from "../views";
 
 export const JournalPage = () => {
     return (
         <JournalLayout>
-            <Typography>
-                Est ullamco aliqua labore magna reprehenderit. Ipsum cupidatat
-                eu anim ex exercitation ea sint id esse laboris enim amet
-                consectetur. Minim officia anim do do eu Lorem dolor proident
-                sint ex. Excepteur nisi aliquip pariatur tempor fugiat.
-            </Typography>
-            {/* NOTHINGSELECTED */}
-            {/* NOTEVIEW */}
+            {/* <NothingSelectedView /> */}
+            <NoteView />
         </JournalLayout>
     );
 };

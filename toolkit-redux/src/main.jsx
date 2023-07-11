f484016e2a4c4ba06ff6deb9d8255bb06f22ab8d
AlexDevv <104278094+AlexProcess@users.noreply.github.com>
2023-07-11 19:50:14 +0200

    Pokemon Slice

--- toolkit-redux/src/main.jsx
+++ toolkit-redux/src/main.jsx
@@ -4,11 +4,11 @@ import { Provider } from "react-redux";
 import App from "./App";
 import "./index.css";
 import { store } from "./store/store";
+import { PokemonApp } from "./PokemonApp";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
-        <Provider store={store}>
-            <App />
-        </Provider>
+        <Provider store={store}>{/* <App /> */}</Provider>
+        <PokemonApp />
     </React.StrictMode>
 );

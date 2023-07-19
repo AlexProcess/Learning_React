54829e21e8cfb8f0dd8f53729a57b33b3a747443
AlexDevv <104278094+AlexProcess@users.noreply.github.com>
2023-07-19 13:53:57 +0200

    personalizar el cuadro del evento

--- calendar/src/calendar/pages/CalendarPage.jsx
+++ calendar/src/calendar/pages/CalendarPage.jsx
@@ -3,9 +3,9 @@ import { Calendar } from "react-big-calendar";
 import { addHours } from "date-fns";
 import "react-big-calendar/lib/css/react-big-calendar.css";
 import { getMessagesES, localizer } from "../../helpers";
+import { CalendarEvent } from "../components/CalendarEvent";
 
 const eventStyleGetter = (event, start, end, isSelected) => {
-    console.log({ event, start, end, isSelected });
     const style = {
         backgroundColor: "#347CF7",
         borderRadius: "0px",
@@ -46,6 +46,9 @@ export const CalendarPage = () => {
                 style={{ height: "calc(100vh - 60px)" }}
                 messages={getMessagesES()}
                 eventPropGetter={eventStyleGetter}
+                components={{
+                    event: CalendarEvent,
+                }}
             />
         </>
     );

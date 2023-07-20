22ae8cbd6f3f6ddb8b084f90f4d0539a0b1bc9b2
AlexDevv <104278094+AlexProcess@users.noreply.github.com>
2023-07-20 21:32:14 +0200

    contenido del modal[para fixear]

--- calendar/src/calendar/components/CalendarModal.jsx
+++ calendar/src/calendar/components/CalendarModal.jsx
@@ -1,5 +1,7 @@
+import { addHours } from "date-fns";
 import { useState } from "react";
 import ReactModal from "react-modal";
+import DatePicker from "react-datepicker";
 
 export const CalendarModal = () => {
     const onCloseModal = () => {
@@ -20,6 +22,27 @@ export const CalendarModal = () => {
 
     const [isOpen, setIsOpen] = useState(true);
 
+    const [formValues, setFormValues] = useState({
+        title: "Alex",
+        notes: "Tercero",
+        start: new Date(),
+        end: addHours(new Date(), 2),
+    });
+
+    const onInputChanged = ({ target }) => {
+        setFormValues({
+            ...formValues,
+            [target.name]: target.value,
+        });
+    };
+
+    const onDateChanged = (event, changing) => {
+        setFormValues({
+            ...formValues,
+            [changing]: event,
+        });
+    };
+
     return (
         <ReactModal
             isOpen={isOpen}
@@ -28,15 +51,71 @@ export const CalendarModal = () => {
             overlayClassName={"modal-fondo"}
             closeTimeoutMS={200}
         >
-            <h1>Fakin croissant</h1>
+            <h1> Nuevo evento </h1>
             <hr />
-            <p>
-                Magna anim eiusmod veniam nisi aliquip ex in aute mollit id
-                deserunt. Tempor labore irure ad aute laboris et id adipisicing
-                quis esse. Consectetur veniam dolor veniam labore veniam officia
-                voluptate tempor. Ea eu ut reprehenderit deserunt fugiat.
-                Eiusmod nisi tempor enim est aute mollit.
-            </p>
+            <form className="container">
+                <div className="form-group mb-2">
+                    <label>Fecha y hora inicio</label>
+                    <DatePicker
+                        selected={formValues.start}
+                        onChange={(event) => onDateChanged(event, "start")}
+                        className="form-control"
+                        dateFormat="Pp"
+                    />
+                    <input
+                        className="form-control"
+                        placeholder="Fecha inicio"
+                    />
+                </div>
+
+                <div className="form-group mb-2">
+                    <label>Fecha y hora fin</label>
+                    <input
+                        className="form-control"
+                        placeholder="Fecha inicio"
+                    />
+                </div>
+
+                <hr />
+                <div className="form-group mb-2">
+                    <label>Titulo y notas</label>
+                    <input
+                        type="text"
+                        className="form-control"
+                        placeholder="Título del evento"
+                        name="title"
+                        autoComplete="off"
+                        value={formValues.title}
+                        onChange={onInputChanged}
+                    />
+                    <small id="emailHelp" className="form-text text-muted">
+                        Una descripción corta
+                    </small>
+                </div>
+
+                <div className="form-group mb-2">
+                    <textarea
+                        type="text"
+                        className="form-control"
+                        placeholder="Notas"
+                        rows="5"
+                        name="notes"
+                        value={formValues.notes}
+                        onChange={onInputChanged}
+                    ></textarea>
+                    <small id="emailHelp" className="form-text text-muted">
+                        Información adicional
+                    </small>
+                </div>
+
+                <button
+                    type="submit"
+                    className="btn btn-outline-primary btn-block"
+                >
+                    <i className="far fa-save"></i>
+                    <span> Guardar</span>
+                </button>
+            </form>
         </ReactModal>
     );
 };

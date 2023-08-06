1cdc20c10affbe52ded535bdf09cfdac006d6373
AlexDevv <104278094+AlexProcess@users.noreply.github.com>
2023-08-06 12:09:54 +0200

    variables de entorno y carpeta public

--- calendar-backend/index.js
+++ calendar-backend/index.js
@@ -1,4 +1,7 @@
 const express = require("express");
+require("dotenv").config();
+
+console.log(process.env);
 
 //Crear el servidor de express
 
@@ -6,14 +9,16 @@ const app = express();
 
 //RUTAS
 
-app.get("/", (req, res) => {
-    console.log("se requiere el slash");
-    res.json({
-        ok: true,
-    });
-});
+// app.get("/", (req, res) => {
+//     res.json({
+//         ok: true,
+//     });
+// });
+
+//Directorio publico
+app.use(express.static("public"));
 
 //Escuchar peticiones
-app.listen(4000, () => {
-    console.log(`Server running on port ${4000}`);
+app.listen(process.env.PORT, () => {
+    console.log(`Server running on port ${process.env.PORT}`);
 });

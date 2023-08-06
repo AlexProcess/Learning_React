0e5bfbb16e8bad819e4eb9a979abd76c05aca5e8
AlexDevv <104278094+AlexProcess@users.noreply.github.com>
2023-08-06 11:31:27 +0200

    config express

--- calendar-backend/index.js
+++ calendar-backend/index.js
@@ -1,1 +1,19 @@
-console.log("Hola mundo!!!");
+const express = require("express");
+
+//Crear el servidor de express
+
+const app = express();
+
+//RUTAS
+
+app.get("/", (req, res) => {
+    console.log("se requiere el slash");
+    res.json({
+        ok: true,
+    });
+});
+
+//Escuchar peticiones
+app.listen(4000, () => {
+    console.log(`Server running on port ${4000}`);
+});

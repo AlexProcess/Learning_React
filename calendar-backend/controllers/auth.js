9fb15b2dfa91e8488222205ed98b435ee2f80df2
AlexDevv <104278094+AlexProcess@users.noreply.github.com>
2023-08-09 20:31:43 +0200

    Login de usuario

--- calendar-backend/controllers/auth.js
+++ calendar-backend/controllers/auth.js
@@ -29,6 +29,7 @@ const crearUsuario = async (req, res = response) => {
             name: usuario.name,
         });
     } catch (error) {
+        console.log(error);
         res.status(500).json({
             ok: false,
             msg: "Por favor hable con el administrador",
@@ -36,10 +37,43 @@ const crearUsuario = async (req, res = response) => {
     }
 };
 
-const loginUsuario = (req, res = response) => {
+const loginUsuario = async (req, res = response) => {
     //Manejo de errores
 
     const { name, email, password } = req.body;
+
+    try {
+        const usuario = await Usuario.findOne({ email });
+
+        if (!usuario) {
+            return res.status(400).json({
+                ok: false,
+                msg: "El usuario ya existe con ese email",
+            });
+        }
+        //CONFIRMAR LOS PASSWORDS
+        const validPassword = bcrypt.compareSync(password, usuario.password);
+
+        if (!validPassword) {
+            return res.status(400).json({
+                ok: false,
+                msg: "Password Incorrecto",
+            });
+        }
+
+        //Generar JWT
+        res.json({
+            ok: true,
+            uid: usuario.id,
+            name: usuario.name,
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            ok: false,
+            msg: "Por favor hable con el administrador",
+        });
+    }
 
     res.json({
         ok: true,

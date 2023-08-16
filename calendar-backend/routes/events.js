4ceedb858c3c98f28014650131b78a09fa64bd8d
AlexDevv <104278094+AlexProcess@users.noreply.github.com>
2023-08-16 03:34:53 +0200

    Validar campos necesarios

--- calendar-backend/routes/events.js
+++ calendar-backend/routes/events.js
@@ -5,7 +5,9 @@ EVENTS ROUTES
 
 const { Router } = require("express");
 router = Router();
-
+const { check } = require("express-validator");
+const { isDate } = require("../helpers/isDate");
+const { validarCampos } = require("../middlewares/validar-campos");
 const { validarJWT } = require("../middlewares/validar-jwt");
 const {
     EliminarEvento,
@@ -19,7 +21,16 @@ router.use(validarJWT);
 router.get("/", getEventos);
 
 //Crear nuevo evento
-router.post("/", CrearEvento);
+router.post(
+    "/",
+    [
+        check("title", "El titulo es obligatorio").not().isEmpty(),
+        check("start", "Fecha de inicio es obligatoria").custom(isDate),
+        check("end", "La fecha de fin es obligatoria").custom(isDate),
+        validarCampos,
+    ],
+    CrearEvento
+);
 
 //Actualizar evento
 router.put("/:id", actualizarEvento);

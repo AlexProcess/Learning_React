4ffd78cf58bf09a59478067c067558e666bf60cb
AlexDevv <104278094+AlexProcess@users.noreply.github.com>
2023-08-16 04:14:32 +0200

    Grabar el evento en la base de datos

--- calendar-backend/controllers/events.js
+++ calendar-backend/controllers/events.js
@@ -1,5 +1,5 @@
 const { response } = require("express");
-
+const Evento = require("../models/Evento");
 const getEventos = (req, res) => {
     res.json({
         ok: true,
@@ -7,14 +7,29 @@ const getEventos = (req, res) => {
     });
 };
 
-const CrearEvento = (req, res) => {
-    //Verificcar que tenga el evento
-    console.log(req.body);
+const CrearEvento = async (req, res = response) => {
+    // Verificar que tenga el evento
+    const evento = new Evento(req.body);
 
-    res.json({
-        ok: true,
-        msg: "CrearEvento",
-    });
+    try {
+        // Asignar el usuario antes de guardar el evento
+        evento.user = req.uid;
+
+        const eventoGuardado = await evento.save();
+
+        res.json({
+            ok: true,
+            evento: eventoGuardado,
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            ok: false,
+            msg: "Hable con el administrador",
+        });
+    }
+
+    console.log(req.body);
 };
 
 const actualizarEvento = (req, res) => {
